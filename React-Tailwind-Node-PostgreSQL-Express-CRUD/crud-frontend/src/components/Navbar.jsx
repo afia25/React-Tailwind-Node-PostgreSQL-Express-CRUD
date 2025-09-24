0e71a28c9dcf4c63bf7bf9ac9070881544d750e4
afia25 <18201033@uap-bd.edu>
2025-09-25 01:52:51 +0600

    React-Tailwind-Node-PostgreSQL-Express-CRUD

---
+++ React-Tailwind-Node-PostgreSQL-Express-CRUD/crud-frontend/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+export default function Navbar({onOpen, onSearch}) {
+   const handleSearchChange = (eve) => {
+      onSearch(eve.target.value);
+   };
+
+    return (
+        <>
+        <div className="navbar bg-base-100 shadow-sm p-4">
+  <div className="navbar-start">
+    <a className="btn btn-ghost text-xl">Clients</a>
+  </div>
+  <div className="navbar-center">
+    <div className="form-control"> 
+    <input type="text" placeholder="Search" onChange={handleSearchChange} className="input input-bordered w-48 md:w-auto" />
+    </div>  
+  </div>
+  <div className="navbar-end">
+    <a className="btn btn-primary" onClick={onOpen}>Add Client</a>
+  </div>
+</div>
+        </>
+    )
+}
