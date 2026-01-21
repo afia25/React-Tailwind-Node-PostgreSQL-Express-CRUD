3a245894dcb198575989f8cd060c064041510cc4
afia25 <afia6402@gmail.com>
2026-01-21 10:30:00 +0600

    React Project

---
+++ React-Tailwind-Node-PostgreSQL-Express-CRUD/crud-frontend/src/App.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import Modalform from './components/Modalform'
+import Navbar from './components/Navbar'
+import Tablelist from './components/Tablelist'
+import axios from 'axios';
+
+function App() { 
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalMode, setmodalMode] = useState('add');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [clientData, setClientData] = useState(null);
+  const [tableData, setTableData] = useState([]);
+
+
+  const fetchClients = async () =>{
+        try {
+          const response = await axios.get('http://localhost:3000/api/clients');
+          setTableData(response.data);
+        } 
+        catch (err) {
+          setError(err.message);
+        }
+      };  
+
+
+  useEffect(() => {
+      fetchClients(); 
+    }, []);    
+
+
+  const handleOpen = (mode, client) => {
+    setClientData(client);
+    setmodalMode(mode);
+    setIsOpen(true);
+  }
+
+  const handleSubmit = async (newClientData) => {
+      if (modalMode === 'add'){
+        try{
+          const response = await axios.post('http://localhost:3000/api/clients', newClientData);
+          console.log('Client added:', response.data);
+          setTableData((prevData) => [...prevData, response.data]);
+        }
+        catch (err) {
+          console.error('Error adding client:', err);
+        }
+        console.log('model mode Add');
+      }
+
+      else {
+        console.log('Updating client with ID:', clientData.id); // Log the ID being
+        try {
+            const response = await axios.put(`http://localhost:3000/api/clients/${clientData.id}`, newClientData);
+            console.log('Client updated:', response.data);
+            setTableData((prevData) =>
+              prevData.map((client) => (client.id === clientData.id ? response.data : client))
+        );
+      } 
+        catch (error) {
+            console.error('Error updating client:', error);
+        }
+    }
+  }
+
+
+  return ( 
+  <> 
+  <Navbar onOpen={() => handleOpen('add')} onSearch={setSearchTerm} />
+  <Tablelist setTableData={setTableData} tableData={tableData} handleOpen = {handleOpen} searchTerm={searchTerm} />
+  <Modalform 
+  isOpen = {isOpen} onSubmit = {handleSubmit} 
+  onClose = {() => setIsOpen(false)} 
+  mode = {modalMode} clientData={clientData} />
+
+  </> 
+  ) 
+} 
+
+export default App
+
+
